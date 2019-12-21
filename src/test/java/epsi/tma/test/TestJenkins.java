55b9ef1ea09f5bb432d08f6472a1bacdd15cc102
Florentin <florentin.dekyndt@gmail.com>
2019-12-21 16:42:06 +0100

    test with false TestJenkins file

---
+++ src/test/java/epsi/tma/test/TestJenkins.java
@@ -0,0 +1,14 @@
+package epsi.tma.test;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+public class TestJenkins extends TestCase {
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( false );
+    }
+}
