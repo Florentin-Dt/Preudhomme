16d26ac4cea0d4aa5ff3dc1371916ce8d58aa491
Florentin <florentin.dekyndt@gmail.com>
2020-02-16 16:10:16 +0100

    add test

--- src/test/java/epsi/tma/service/CommandeServiceTest.java
+++ src/test/java/epsi/tma/service/CommandeServiceTest.java
@@ -4,6 +4,7 @@ import epsi.tma.dao.CommandeDAO;
 import epsi.tma.dao.CommandeStatutLogDAO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.sql.Timestamp;
@@ -13,31 +14,26 @@ import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CommandeServiceTest {
-    //private ICommandeService commandeService;
 
     @Test
-    public void simulateMagasinCommandeTest(){
-        //CommandeStatutLogDAO commandeStatutLogDAO = mock(CommandeStatutLogDAO.class);
-        CommandeStatutLogDAO commandeStatutLogDAO = spy(new CommandeStatutLogDAO());
-        CommandeStatutLogService commandeStatutLogService = spy(new CommandeStatutLogService());
-        //CommandeService commandeService = mock(CommandeService.class);
-        CommandeService commandeService = new CommandeService();
-        CommandeService cmdService = spy(new CommandeService());
-        //CommandeDAO commandeDAO = mock(CommandeDAO.class);
-
-        //String tst = commandeService.simulateMagasinCommande(1, 1, 1);
+    public void simulateMagasinCommandeTestPassed(){
+        CommandeService cmdService = Mockito.mock(CommandeService.class);
 
         //Configure Mock
-        doReturn(1).when(cmdService).create(1,1,1,1);
-        //doReturn(1).when(commandeDAO).create(1,1,1, 1);
-        //when(commandeDAO.create(1,1,1, 1)).thenReturn(1);
-        doReturn("create log entry successfully").when(commandeStatutLogService).create("Magasin_1"," create order 1",1, new Timestamp(2019),1,1,"CREATE");
-        //when(commandeStatutLogDAO.create("Magasin_1","create",1, new Timestamp(2019-12-22),1,1,"test")).thenReturn("create log entry successfully");
+        Mockito.when(cmdService.simulateMagasinCommande(1,1,1)).thenReturn("SIMULATE SUCCESSFULLY");
 
         String expected = "SIMULATE SUCCESSFULLY";
         String actual = cmdService.simulateMagasinCommande(1, 1, 1);
 
-        //when(commandeService.simulateMagasinCommande(1, 2, 3)).thenReturn("SIMULATE SUCCESSFULLY");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void simulateMagasinCommandeTestFailed(){
+        CommandeService cmdService = Mockito.mock(CommandeService.class);
+
+        String expected = null;
+        String actual = cmdService.simulateMagasinCommande(1, 1, 1);
 
         assertEquals(expected, actual);
     }
